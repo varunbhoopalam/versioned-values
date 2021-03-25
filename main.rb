0942a67de65a5e67bc6517ff0001528780f8245c
varunbhoopalam <bhoopalam.varun@gmail.com>
2021-03-25 18:37:45 -0500

    Default behavior to go through init cycle only if .values does not exist with version 1-0

--- main.rb
+++ main.rb
@@ -3,5 +3,9 @@ $LOAD_PATH << '.'
 require 'init'
 
 if __FILE__ == $0
-  Init.init
+  if not Dir.exists?(".values/1-0")
+    Init.init
+  else
+    puts("To Do: Pretty Print Latest Value Hierarchy")
+  end
 end

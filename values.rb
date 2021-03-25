57b00764f8b58b671eb11308920f1b24600d253c
varunbhoopalam <bhoopalam.varun@gmail.com>
2021-03-25 18:18:44 -0500

    Init method implemented

--- values.rb
+++ values.rb
@@ -1,6 +1,123 @@
 #!/usr/bin/ruby
 require 'readline'
-while input = Readline.readline("> ", true)
-  break if input == "exit"
-  puts(input)
+require 'fileutils'
+require 'json'
+require 'date'
+
+def cannot_initialize_directory
+  if Dir.exists?(".values") == true
+    puts("It looks like the ./values folder is already being used in this directory, can we overwrite it? Y to overwrite")
+    while input = Readline.readline("> ", true)
+      break
+    end
+    
+    if input != "Y"
+      puts("Will not overwrite existing ./values directory and exiting application")
+      return true
+    end
+  
+  end
+  return false
 end
+
+def parsed_unique_principles
+  principles = []
+  puts("Please provide principles in a list with commas in-between like so: Accountability, Empathy, Integrity, Joyfulness, Initiative")
+  while input = Readline.readline("> ", true)
+    principles = input.split(",")
+    break if principles.length() > 0
+    puts("I couldn't understand what you wrote")
+    puts(input)
+    puts("Please provide principles in a list with commas in-between like so: Accountability, Empathy, Integrity, Joyfulness, Initiative")
+  end
+  puts("\nWonderful principles!")
+  result = principles.uniq.map { |p| p.strip }
+  puts(result.join("\n"))
+  return result
+
+end
+
+def get_principles
+  puts("\nLet's start with what your principles are!")
+  puts("Principles are universal truths, typically nouns, that live at the center of our being.")
+  puts("Examples of principles are Accountability, Empathy, Integrity, Joyfulness, and Initiative.")
+  puts("Can you give a list of principles that are central to how you live?\n")
+  return parsed_unique_principles
+end
+
+def get_value_of_principle(principle)
+  puts("\nWhat is your interpretation of this principle: #{principle}")
+  while input = Readline.readline("> ", true)
+    break if input.length > 0
+  end
+  puts("\nPrinciple: #{principle} - Value: #{input.strip}")
+  return input.strip
+end
+
+def add_values_to_principles(principles)
+  puts("\nValues are personal, internal, arguable guiding tools that help us navigate a complex changing world and make decisions.")
+  puts("Values are our interpetation of our principles.")
+  puts("You cannot practice the principle 'Integrity', but you can practice the value of 'doing the right thing.'")
+  return principles.map { |p|
+    { principle: p, value: get_value_of_principle(p)}
+  }
+end
+
+def order_principles(principles_and_values)
+  puts("\nNow it's time to turn the value list into a value hierarchy!")
+  puts("Type the name of the most important principle until there are no principles left!\n")
+  puts("Remaining Principles:")
+  puts(principles_and_values.map {|pv| "#{pv[:principle]}\n"})
+  principle_and_value_list = []
+  i = 1
+  while input = Readline.readline("> ", true)  
+    maybePAndV = principles_and_values.select { |hash| hash[:principle].downcase == input.strip.downcase }
+    
+    if not maybePAndV.empty?
+      hash = maybePAndV[0]
+      hash[:index] = i
+      principle_and_value_list.push(hash)
+      principles_and_values = principles_and_values.select { |hash| hash[:principle].downcase != input.strip.downcase }
+      i = i + 1
+    end
+    
+    break if principles_and_values.empty?
+
+    puts("Remaining Principles:")
+    puts(principles_and_values.map {|pv| "#{pv[:principle]}\n"})
+  
+  end
+  return principle_and_value_list
+end
+
+def write_to_disk(principle_hashes)
+  FileUtils.remove_dir(".values")
+  FileUtils.mkdir_p(".values/1-0")
+  tempHash = {principles: principle_hashes}
+  File.open(".values/1-0/values.json","w") do |f|
+    f.write(tempHash.to_json)
+  end
+  File.open(".values/1-0/release-notes.txt","w") do |f|
+    f.write("First release!")
+  end
+  metadata = { timestamp: Time.now.to_i, version: "1.0"}
+  File.open(".values/1-0/metadata.json","w") do |f|
+    f.write(metadata.to_json)
+  end
+end
+
+def init
+  return if cannot_initialize_directory
+  principles = get_principles
+  principles_and_values = add_values_to_principles(principles)
+  ordered_principles = order_principles(principles_and_values)
+  write_to_disk(ordered_principles)
+end
+
+if __FILE__ == $0
+  init
+end
+
+
+## Init
+## Finally write the principle session to disk by creating a directory ./.values and inside values a folder named 1-0 with the appropriate files

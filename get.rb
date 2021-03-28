4223a731e3bef76f286ddb537c485f3020395aac
varunbhoopalam <bhoopalam.varun@gmail.com>
2021-03-27 22:39:05 -0500

    Implement print single version

--- get.rb
+++ get.rb
@@ -6,10 +6,29 @@ module Get
   def self.get_latest
     return get_all_versions.max_by{ |value| value.get_version.get_comparable_version }
   end
-  def self.get_all_descending
-    return get_all_versions.sort_by { |value| value.get_version.get_comparable_version }.reverse!
+  
+  def self.get_all
+    return get_all_versions.sort_by { |value| value.get_version.get_comparable_version }
   end
+  
+  def self.get_version_list
+    return Dir.children(".values")
+  end
+  
+  def self.print_version(version)
+    if get_version_list.any? { |v| v == version }
+      v = VersionedValue.new(version)
+      v.get_version.print_version
+      v.get_values.print
+    else
+      puts("Could not find version: #{version}")
+      puts("Please supply version in this format '1-0'")
+    end
+  end
+  
   private_class_method def self.get_all_versions
-    return Dir.children(".values").map { |d| VersionedValue.new(d) }
+    return get_version_list.map { |d| VersionedValue.new(d) }
   end
+  
+
 end
--- main.rb
+++ main.rb
@@ -11,12 +11,19 @@ if __FILE__ == $0
   elsif input_array.any? {|a| a == "-u"}
     Update.update_flow
   elsif input_array.any? {|a| a == "-r"}
-    versions = Get.get_all_descending
+    versions = Get.get_all
     for version in versions
-      puts("---------------")
       version.get_version.print_version
       puts(version.get_release_notes)
+      puts("---------------")
     end
+  elsif input_array.any? {|a| a == "-l"}
+    versions = Get.get_version_list
+    for version in versions
+      puts(version)
+    end
+  elsif input_array.any? {|a| a == "-v"}
+    Get.print_version(input_array[1])
   else
     latest = Get.get_latest
     latest.get_version.print_version

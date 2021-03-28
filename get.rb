b402120ddc9f97398d3e44ab936d73b24f998300
varunbhoopalam <bhoopalam.varun@gmail.com>
2021-03-27 22:44:37 -0500

    Small refactors to move logic from the main script to service

--- get.rb
+++ get.rb
@@ -10,9 +10,31 @@ module Get
   def self.get_all
     return get_all_versions.sort_by { |value| value.get_version.get_comparable_version }
   end
+
+  def self.print_version_list
+    versions = Get.get_version_list
+    for version in versions
+      puts(version)
+    end
+  end
+
+  def self.print_release_notes
+    versions = Get.get_all
+    for version in versions
+      version.get_version.print_version
+      puts(version.get_release_notes)
+      puts("---------------")
+    end
+  end
   
   def self.get_version_list
     return Dir.children(".values")
+  end
+
+  def self.print_latest
+    latest = Get.get_latest
+    latest.get_version.print_version
+    latest.get_values.print
   end
   
   def self.print_version(version)
@@ -22,7 +44,9 @@ module Get
       v.get_values.print
     else
       puts("Could not find version: #{version}")
-      puts("Please supply version in this format '1-0'")
+      puts("Please supply version in this format: 1-0")
+      puts("Here are the valid versions in this app")
+      print_version_list
     end
   end
   
--- main.rb
+++ main.rb
@@ -11,22 +11,12 @@ if __FILE__ == $0
   elsif input_array.any? {|a| a == "-u"}
     Update.update_flow
   elsif input_array.any? {|a| a == "-r"}
-    versions = Get.get_all
-    for version in versions
-      version.get_version.print_version
-      puts(version.get_release_notes)
-      puts("---------------")
-    end
+    Get.print_release_notes
   elsif input_array.any? {|a| a == "-l"}
-    versions = Get.get_version_list
-    for version in versions
-      puts(version)
-    end
+    Get.print_version_list
   elsif input_array.any? {|a| a == "-v"}
     Get.print_version(input_array[1])
   else
-    latest = Get.get_latest
-    latest.get_version.print_version
-    latest.get_values.print
+    Get.print_latest
   end
 end
